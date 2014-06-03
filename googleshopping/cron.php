bb7b5cb4d05ef040207e45c48c7ee74d1943f298
Jordi Martin <jordimartin@gmail.com>
2014-06-03 17:43:37 +0200

    Update cron.php

--- googleshopping/cron.php
+++ googleshopping/cron.php
@@ -2,7 +2,7 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
-include(dirname(__FILE__).'/ps_googleshopping.php');
+include(dirname(__FILE__).'/googleshopping.php');
 
 $module=new GoogleShopping();
 $module->generateFileList();
