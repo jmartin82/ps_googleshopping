888b96ff92b01429bcf1880d42767956820430b6
Jordi Martin <jordimartin@gmail.com>
2014-09-14 22:28:58 +0200

    Added ASF headers

--- googleshopping/googleshopping.php
+++ googleshopping/googleshopping.php
@@ -1,17 +1,23 @@
 <?php
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 if (!defined('_PS_VERSION_'))
     exit;
-
-/*
- * Google Shopping
- * @prestashop
- * Version 1.5.31
- * Jordi Martin (jordimartin@gmail.com)
- * API
- * https://developers.google.com/shopping-content/getting-started/requirements-products
- * Based on idea:
- * (http://www.igwane.com/fr/license)
- */
 
 class GoogleShopping extends Module
 {
@@ -23,8 +29,8 @@ class GoogleShopping extends Module
     {
         $this->name    = 'googleshopping';
         $this->tab     = 'export';
-        $this->version = '1.2';
-        $this->author  = '@jordi_martin';
+        $this->version = '1.3';
+        $this->author  = 'https://github.com/jmartin82/ps_googleshopping';
         
         parent::__construct();
         
