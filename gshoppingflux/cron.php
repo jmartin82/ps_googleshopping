55338d52fc8807c30f69488cd29c5fbb6162db9d
diMooz <dimitri.mestdagh@laposte.net>
2014-11-17 23:08:47 +0100

    Correction

--- gshoppingflux/cron.php
+++ gshoppingflux/cron.php
@@ -4,8 +4,9 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/gshoppingflux.php');
 
-$module=new GoogleShopping();
-$module->generateFileList();
+$module=new GShoppingFlux();
+$shop_id = Shop::getContextShopID();
+$module->generateShopFileList($shop_id);
 die ('OK');
 
 ?>
