c2071514dfbb747072dbb5e6420b7811febc7826
diMooz <dimitri.mestdagh@laposte.net>
2014-11-17 23:04:32 +0100

    correction

--- gshoppingflux/cron.php
+++ gshoppingflux/cron.php
@@ -2,7 +2,7 @@
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
-include(dirname(__FILE__).'/googleshopping.php');
+include(dirname(__FILE__).'/gshoppingflux.php');
 
 $module=new GoogleShopping();
 $module->generateFileList();
